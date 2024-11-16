c47dd853f9f66022f755fccb46828837660777e7
Yousef Na <yousefnasr0541@gmail.com>
2024-11-16 16:52:30 +0200

    add: second grid elment

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,8 +3,9 @@ import MaxWidthWrapper from "@/components/max-width-wrapper"
 import CallButton from "@/components/call-button"
 import { Check } from "lucide-react"
 import MockDiscordUI from "@/components/mock-discord-ui"
-import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
+import { AnimatedList } from "@/components/ui/animated-list"
 import { DiscordMessage } from "@/components/discord-message"
+import Image from "next/image"
 
 export default function page() {
   return (
@@ -50,6 +51,7 @@ export default function page() {
           </div>
         </MaxWidthWrapper>
       </section>
+
       <section className="relative pb-4 bg-brand-25">
         <div className="absolute inset-0 bottom-24 top-24 bg-brand-700" />
         <div className="relative mx-auto">
@@ -103,7 +105,77 @@ export default function page() {
           </MaxWidthWrapper>
         </div>
       </section>
-      <section className="relative bg-brand-25"></section>
+
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+          <div>
+            <h2 className="text-center text-base/7 font-semibold text-brand-600">
+              Intuitive Monitoring
+            </h2>
+            <Heading>Stay ahead with real-time Insights</Heading>
+          </div>
+
+          <div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+            {/* first grid element */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text to-brand-950 max-lg:text-center">
+                    Real-Time notification
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Get notified about critical events the moment they happen,
+                    no matter if you&apos;re at home or on the go.
+                  </p>
+                </div>
+
+                <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                  <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                    <Image
+                      src="/phone-screen.webp"
+                      alt="Phone screen displaying app interface"
+                      fill
+                      className="size-full object-cover object-top"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
+            </div>
+
+            {/* first grid element */}
+            <div className="relative max-lg:row-start-1">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text to-brand-950 max-lg:text-center">
+                    Track any Event
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    From new user signups to successful payments, PingPanda
+                    notifies you for all critical events in your Saas.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                  <Image
+                    src="/bento-any-event.webp"
+                    alt="Bento box illustrating event tracking"
+                    width={500}
+                    height={300}
+                    className="w-full max-lg:max-w-xs"
+                  />
+                </div>
+              </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
+            </div>
+
+            {/*  */}
+          </div>
+        </MaxWidthWrapper>
+      </section>
       <section className="relative bg-brand-25"></section>
     </>
   )
