4bf013c685d4e9a606f48cb332be5eddfa02452d
Yousef Na <yousefnasr0541@gmail.com>
2024-11-16 17:26:37 +0200

    add: fourth grid elment

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,31 @@
+import Image from "next/image"
+import { Check } from "lucide-react"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
+
 import Heading from "@/components/heading"
 import MaxWidthWrapper from "@/components/max-width-wrapper"
 import CallButton from "@/components/call-button"
-import { Check } from "lucide-react"
 import MockDiscordUI from "@/components/mock-discord-ui"
 import { AnimatedList } from "@/components/ui/animated-list"
 import { DiscordMessage } from "@/components/discord-message"
-import Image from "next/image"
 
 export default function page() {
+  const codeSnippets = `await fetch("http://localhost:3000/api/v1/events", {
+  method: "POST",
+  body: JSON.stringify({
+    category: "sale",
+    fields: {
+      plan: "PRO",
+      email: "zoe.martinez2001@email.com",
+      amount: 49.00
+    }
+  }),
+  headers: {
+    Authorization: "Bearer <YOUR_API_KEY>"
+  }
+})`
+
   return (
     <>
       <section className="relative py-24 sm:py-32 bg-brand-25">
@@ -197,6 +215,58 @@ export default function page() {
               </div>
 
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+            </div>
+
+            {/* fourth grid element */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-r-[2rem] max-lg:rounded-b-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)] max-lg:rounded-b-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text to-brand-950 max-lg:text-center">
+                    Easy Integration
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Connect PingPanda with your existing workflows in minutes
+                    and call our intuitive logging API from any language.l
+                  </p>
+                </div>
+
+                <div className="relative min-h-[30rem] w-full grow ">
+                  <div className="absolute bottom-0 right-0 left-10 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                      <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                        <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                          PingPanda.js
+                        </div>
+                      </div>
+                    </div>
+                    <div className="overflow-hidden">
+                      <div className="max-h-[30rem]">
+                        <SyntaxHighlighter
+                          style={{
+                            ...oneDark,
+                            'pre[class*="language-"]': {
+                              ...oneDark['pre[class*="language-"]'],
+                              background: "transparent",
+                              overflow: "hidden",
+                            },
+                            'code[class*="language-"]': {
+                              ...oneDark['code[class*="language-"]'],
+                              background: "transparent",
+                              overflow: "hidden",
+                            },
+                          }}
+                          language="typescript"
+                        >
+                          {codeSnippets}
+                        </SyntaxHighlighter>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem] max-lg:rounded-b-[2rem]" />
             </div>
           </div>
         </MaxWidthWrapper>
