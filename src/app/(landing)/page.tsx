8fab144278bb7ca5fa3592e3b52936b5493db1b0
Yousef Na <yousefnasr0541@gmail.com>
2024-11-16 17:00:29 +0200

    add: third grid elment

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -145,7 +145,7 @@ export default function page() {
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
             </div>
 
-            {/* first grid element */}
+            {/* second grid element */}
             <div className="relative max-lg:row-start-1">
               <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
@@ -172,7 +172,32 @@ export default function page() {
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
             </div>
 
-            {/*  */}
+            {/* third grid element */}
+            <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+              <div className="absolute inset-px rounded-lg bg-white " />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text to-brand-950 max-lg:text-center">
+                    Track any Properties
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Add any custom data you like to an event, such as a user
+                    email, a purchase amount or an exceeded quota.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                  <Image
+                    src="/bento-custom-data.webp"
+                    alt="Bento box illustrating custom data tracking"
+                    width={500}
+                    height={300}
+                    className="w-full max-lg:max-w-xs"
+                  />
+                </div>
+              </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
